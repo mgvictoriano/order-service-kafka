222ee6e8f2e4bee0d2262b1079917d4d4ba76351
mgvictoriano <mgvictoriano@live.com>
2023-10-21 17:44:59 -0300

    including annotation
    Signed-off-by: mgvictoriano <mgvictoriano@live.com>

--- producer/src/main/java/com/order/service/api/producer/ProducerApplication.java
+++ producer/src/main/java/com/order/service/api/producer/ProducerApplication.java
@@ -2,7 +2,9 @@ package com.order.service.api.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class ProducerApplication {
 
