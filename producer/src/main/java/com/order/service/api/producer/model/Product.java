031c4570fdc4483818757c612d6b59193c7ee825
mgvictoriano <mgvictoriano@live.com>
2023-10-21 17:45:20 -0300

    adjustments double to BigDecimal
    Signed-off-by: mgvictoriano <mgvictoriano@live.com>

--- producer/src/main/java/com/order/service/api/producer/model/Product.java
+++ producer/src/main/java/com/order/service/api/producer/model/Product.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -19,6 +21,7 @@ public class Product {
     private String name;
 
     @Column(nullable = false)
-    private double unitPrice;
+    private BigDecimal unitPrice;
+
 
 }
