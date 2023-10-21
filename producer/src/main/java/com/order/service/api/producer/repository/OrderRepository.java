a4bb6f00636b2347da6f2b6ac0ad75a29b2f680a
mgvictoriano <mgvictoriano@live.com>
2023-10-21 17:46:24 -0300

    renamed
    Signed-off-by: mgvictoriano <mgvictoriano@live.com>

--- producer/src/main/java/com/order/service/api/producer/repository/OrderRepository.java
+++ producer/src/main/java/com/order/service/api/producer/repository/OrderRepository.java
@@ -1,11 +1,11 @@
 package com.order.service.api.producer.repository;
 
-import com.order.service.api.producer.model.Order;
+import com.order.service.api.producer.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Orders, Long> {
 
 
 }
